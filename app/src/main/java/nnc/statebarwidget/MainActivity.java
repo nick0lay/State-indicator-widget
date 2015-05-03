3b2b97b834d57054cbbec84899752877325a4ad2
Nikolay Nikiforchuk <nikolay.nikiforchuk@stafffinder.jobs>
2015-05-03 10:20:28 +0300

    Initial project setup; circularprogressbar/CircleProgressBar.java - implemented circularprogressbar/CircularImageView.java - implemented Base demo app created

---
+++ app/src/main/java/nnc/statebarwidget/MainActivity.java
@@ -0,0 +1,93 @@
+package nnc.statebarwidget;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+
+import nnc.statebarwidget.circularprogressbar.CircleProgressBar;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private ProgressBar progressBar;
+        private SeekBar seekBar;
+        private CircleProgressBar circleProgressBar;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+            seekBar = (SeekBar) rootView.findViewById(R.id.seekBar);
+            circleProgressBar = (CircleProgressBar) rootView.findViewById(R.id.progressCircular);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    progressBar.setProgress(progress);
+                    circleProgressBar.setProgressWithAnimation(progress);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            return rootView;
+        }
+    }
+}
